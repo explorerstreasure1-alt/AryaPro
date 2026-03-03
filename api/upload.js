23a968ac42238dd123ccd057230f4e9583bf823b
explorerstreasure1-alt <explorerstreasure1@gmail.com>
2026-03-04 01:30:27 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -34,7 +34,7 @@ export default async function handler(request) {
       });
     }
 
-    // Dosyayı Vercel Blob'a yükle
+    // Token environment variable'dan gelir
     const blob = await put(file.name, file, {
       access: 'public',
       token: process.env.BLOB_READ_WRITE_TOKEN,
