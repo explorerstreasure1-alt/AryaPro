76d1a85976358a5d0eff2d5b9c4a38637ea6e755
explorerstreasure1-alt <explorerstreasure1@gmail.com>
2026-03-04 03:03:03 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,4 @@
-const formData = new FormData();
-formData.append('file', new File(['test'], 'test.txt', { type: 'text/plain' }));
-fetch('/api/upload', { method: 'POST', body: formData })
-  .then(r => r.json())
-  .then(console.log)
-  .catch(console.error);
+const blob = await put(file.name, file, {
+  access: 'public',
+  addRandomSuffix: true,
+});
